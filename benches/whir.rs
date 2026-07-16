3e4f3aaeda0a12da076597b5bc3bfa3846b269d6
zkfriendly <zkfriendly@proton.me>
2026-07-16 11:32:34 +0200

    feat: add divan benchmark for whir as polynomial commitment scheme

--- benches/whir.rs
+++ benches/whir.rs
@@ -7,7 +7,7 @@ use whir::{
     algebra::{
         embedding::{Basefield, Embedding},
         fields::Field64_3,
-        linear_form::LinearForm,
+        linear_form::{Evaluate, LinearForm, MultilinearExtension},
     },
     buffer::{ActiveBuffer, BufferOps},
     cmdline_utils::AvailableHash::Blake3,
@@ -20,8 +20,19 @@ use whir::{
 static ALLOC: AllocProfiler = AllocProfiler::system();
 
 const SIZES: &[u64] = &[1 << 16, 1 << 18, 1 << 20, 1 << 21];
+const PROTOCOL_PARAMS: ProtocolParameters = ProtocolParameters {
+    security_level: 128,
+    pow_bits: 20,
+    initial_folding_factor: 4,
+    folding_factor: 4,
+    unique_decoding: false,
+    starting_log_inv_rate: 1,
+    batch_size: 1,
+    hash_id: Blake3.hash_id(),
+};
 
 type WhirEmbedding = Basefield<Field64_3>;
+type WhirTarget = <WhirEmbedding as Embedding>::Target;
 
 /// run whir as low degree test, no constraints(linear forms) or evaluations
 #[divan::bench(args = SIZES)]
@@ -31,21 +42,52 @@ fn whir_ldt(bencher: Bencher, size: u64) {
             let vector = (0..size)
                 .map(<WhirEmbedding as Embedding>::Source::from)
                 .collect::<Vec<_>>();
-            let input = ActiveBuffer::from_slice(&vector);
-            let params = ProtocolParameters {
-                security_level: 128,
-                pow_bits: 20,
-                initial_folding_factor: 4,
-                folding_factor: 4,
-                unique_decoding: false,
-                starting_log_inv_rate: 1,
-                batch_size: 1,
-                hash_id: Blake3.hash_id(),
-            };
-            (input, params)
+            ActiveBuffer::from_slice(&vector)
         })
-        .bench_values(|(input, params)| {
-            run_whir::<WhirEmbedding>(&input, &params, vec![], Cow::Borrowed(&[]));
+        .bench_values(|input| {
+            run_whir::<WhirEmbedding>(&input, &PROTOCOL_PARAMS, vec![], Cow::Borrowed(&[]));
+        });
+}
+
+/// run whir as polynomial commitment scheme
+#[divan::bench(args = SIZES)]
+fn whir_pcs(bencher: Bencher, size: u64) {
+    bencher
+        .with_inputs(|| {
+            let num_variables = size.trailing_zeros() as usize;
+            let vector = (0..size)
+                .map(<WhirEmbedding as Embedding>::Source::from)
+                .collect::<Vec<_>>();
+            let input = ActiveBuffer::from_slice(&vector);
+            let embedding = WhirEmbedding::default();
+
+            let points: Vec<_> = (0..2u64)
+                .map(|i| vec![WhirTarget::from(i); num_variables])
+                .collect();
+
+            let mut evaluations = Vec::new();
+            for point in &points {
+                let linear_form = MultilinearExtension::new(point.clone());
+                evaluations.push(linear_form.evaluate(&embedding, &vector));
+            }
+
+            let linear_forms: Vec<Box<dyn LinearForm<WhirTarget>>> = points
+                .iter()
+                .map(|p| {
+                    Box::new(MultilinearExtension::new(p.clone()))
+                        as Box<dyn LinearForm<WhirTarget>>
+                })
+                .collect();
+
+            (input, linear_forms, evaluations)
+        })
+        .bench_values(|(input, linear_forms, evaluations)| {
+            run_whir::<WhirEmbedding>(
+                &input,
+                &PROTOCOL_PARAMS,
+                linear_forms,
+                Cow::Borrowed(&evaluations),
+            );
         });
 }
 
